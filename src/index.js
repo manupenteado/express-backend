5917c58dae9693ea3793975332e7ea12c0ae9068
Emanuele Penteado <163652019+manupenteado@users.noreply.github.com>
2025-04-29 11:29:44 +0000

    feat: add initial express backend files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {console.log('Server is running on port 3000');});
+//converte o que a gente recebe no servidor para um obj javascript
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send({message: 'Hello everyone!'});
+    });
+
+//se a variavel nao for definida, ele vai usar o 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {console.log(`Server is running on port http://localhost:${PORT}/`);});
