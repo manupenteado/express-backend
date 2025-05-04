b53a6058ca27018f43deec1c0188bf2d8cd2b90c
Emanuele Penteado <163652019+manupenteado@users.noreply.github.com>
2025-05-04 01:06:46 +0000

    end of videos: post e get

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 
 const register = async (req, res) =>{
     console.log("registering user:", req.body);
@@ -28,4 +29,44 @@ const register = async (req, res) =>{
 
 };
 
-export default {register};
+const login = async (req, res) => {
+    console.log("logging in user:", req.body);
+
+    //validação
+    if (!req.body || !req.body.username || !req.body.password){
+        return res.status(400).json({message: "Username and password are required!"});
+    }
+
+    const { username, password} = req.body;
+
+    try{
+
+        const user = await User.findOne({
+            username
+        }).select('+password');
+
+        if (!user){
+            console.log("User not found", username);
+            return res.status(400).json ({message: 'User not found'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        console.log ("Password match:", isMatch);
+        if (!isMatch){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+        console.log("User logged in successfully", user.username);
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+
+        return res.status(200).json({message: 'Login successful', token});
+
+
+    } catch (error){
+        console.error("Error logging user:", error);
+        return res.status(500). json({message: `Error logging user: ${error}`});
+    }
+
+};
+
+export default {register, login};
